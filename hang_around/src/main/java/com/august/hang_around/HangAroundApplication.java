e32559e29c202d261a9e6e3bdd39784cbc757079
“Augsut-Rushme” <864011713@qq.com>
2021-07-01 13:31:04 +0800

    init project

---
+++ hang_around/src/main/java/com/august/hang_around/HangAroundApplication.java
@@ -0,0 +1,16 @@
+package com.august.hang_around;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 战神
+ */
+@SpringBootApplication
+public class HangAroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangAroundApplication.class, args);
+	}
+
+}
